23aab0a80cb5b2cd138b012f1d869fea6dcb8802
Kitae's Github <aqua0405@gmail.com>
2025-10-24 00:06:04 +0900

    Subtitle 수정

--- components/MealDetail/Subtitle.js
+++ components/MealDetail/Subtitle.js
@@ -1,6 +1,6 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { StyleSheet, Text, View } from "react-native";
-
 const Subtitle = ({children}) => {
   return (
     <View style={styles.subtitleContainer}>
@@ -9,20 +9,24 @@ const Subtitle = ({children}) => {
   );
 };
 
+Subtitle.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
 export default Subtitle;
 
 const styles = StyleSheet.create({
-  subtitle: {
-    color: "#e2b497",
-    fontSize: 18,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
   subtitleContainer: {
     padding: 6,
     margin: 4,
     marginHorizontal: 12,
     borderBottomColor: "#e2b497",
     borderBottomWidth: 2,
+  },
+  subtitle: {
+    color: "#e2b497",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
